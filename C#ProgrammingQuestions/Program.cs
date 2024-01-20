376bcfb7b4a74169a5215c09d4fb4e0d0b26c4ed
Rahul <beingunique2000@gmail.com>
2024-01-20 12:39:18 +0530

    string reverse

---
+++ C#ProgrammingQuestions/Program.cs
@@ -0,0 +1,34 @@
+﻿using ReverseStringnameSpace;
+
+
+namespace ProgrammingQuestions
+{
+    class Program
+    {
+        static Dictionary<int, string> selectionOption = new Dictionary<int, string>()
+        {
+            {1,"ReverseString" }
+        };
+        static void Main()
+        {
+            Console.WriteLine("Welcome to C# Programming questions example");
+            Console.WriteLine("Select the Programming Question as per below selecion option.Type the option number.\n");
+            foreach( var selection in selectionOption )
+            {
+                Console.WriteLine(selection.Key+" - "+selection.Value);
+            }
+            int value = 0;
+            if(int.TryParse(Console.ReadLine(),out value))
+            {
+                switch(value)
+                {
+                    case 1:
+                        ReverseString revesreStringObj = new ReverseString();
+                        Console.WriteLine(revesreStringObj.ReverseStringFunctionWithInBuilt());
+                        Console.WriteLine(revesreStringObj.ReverseStringFunctionLoop());
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ReverseString/Class1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * In This we are trying to reverse a given string using 
+ * 1. The inbuilt reverse static method available in the Array class
+ * 2. Using the for loop to iterate thrugh the char array which we crearted from ṭhe string and reversing the same
+*/
+
+
+namespace ReverseStringnameSpace
+{
+    public class ReverseString
+    {
+        string stringValue = "HelloWorld";
+        public string ReverseStringFunctionWithInBuilt()
+        {
+            char[] charArray = stringValue.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public string ReverseStringFunctionLoop()
+        {
+            char[] charArray = stringValue.ToCharArray();
+            char[] newCharArray = new char[charArray.Length];
+            int j = 0;
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                newCharArray[j++] = charArray[i];
+            }
+            return new string(newCharArray);
+        }
+    }
+}
